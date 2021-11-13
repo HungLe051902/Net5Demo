d63a7a829c92af5e0853eccace02db3926ea0366
Le Xuan Hung <43202901+HungLe051902@users.noreply.github.com>
2021-11-13 21:20:24 +0700

    frist cm

---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public interface IInMemItemsRepository
+    {
+        Item GetItem(Guid id);
+        IEnumerable<Item> GetItems();
+    }
+
+    public class InMemItemsRepository : IInMemItemsRepository
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "iron sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "bronze shield", Price = 6, CreatedDate = DateTimeOffset.UtcNow }
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+    }
+}
